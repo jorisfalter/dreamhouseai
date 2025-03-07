ba35802eb8a672d56ceafbfba8418d6bcec9ddd4
Joris Falter <joris@jorisfalter.com>
2025-03-07 12:12:04 +0100

    x

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,38 +1,28 @@
 import mongoose from 'mongoose';
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+declare global {
+  var mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+export async function connectDB() {
+  if (global.mongoose.conn) {
+    return global.mongoose.conn;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(process.env.MONGODB_URI!, opts).then((mongoose) => {
-      return mongoose;
-    });
+  if (!process.env.MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable');
   }
 
-  try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
+  if (!global.mongoose.promise) {
+    global.mongoose.promise = mongoose.connect(process.env.MONGODB_URI);
   }
-
-  return cached.conn;
-}
-
-export default dbConnect; 
+  global.mongoose.conn = await global.mongoose.promise;
+  return global.mongoose.conn;
+} 

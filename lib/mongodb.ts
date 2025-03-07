92703a11f5081e59eb07bb2c902d36148b45f6c9
Joris Falter <joris@jorisfalter.com>
2025-03-07 12:46:52 +0100

    x

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,45 +4,44 @@ declare global {
   var mongoose: {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
-  };
+  } | undefined;
 }
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-const MONGODB_URI = process.env.MONGODB_URI;
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
 
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = {
+    conn: null,
+    promise: null
+  };
 }
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts)
-      .then((mongoose) => {
-        return mongoose;
-      });
-  }
-
   try {
-    cached.conn = await cached.promise;
+    if (cached?.conn) {
+      return cached.conn;
+    }
+
+    if (!cached?.promise) {
+      const opts = {
+        bufferCommands: false,
+      };
+
+      cached!.promise = mongoose.connect(MONGODB_URI, opts);
+    }
+
+    cached!.conn = await cached.promise;
+    return cached.conn;
   } catch (e) {
-    cached.promise = null;
+    cached!.promise = null;
     throw e;
   }
-
-  return cached.conn;
 }
 
 export default dbConnect; 
